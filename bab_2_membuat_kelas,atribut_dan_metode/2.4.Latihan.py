6b83ab11325e420ef4c854ea8097eb334d8f8705
ryku <herykurniawansartono@gmail.com>
2023-09-29 17:49:01 +0700

    update

---
+++ bab_2_membuat_kelas,atribut_dan_metode/2.4.Latihan.py
@@ -0,0 +1,14 @@
+class Anjing:
+    def __init__(self,nama,umur,warna):
+        self.nama = nama
+        self.umur = umur
+        self.warna = warna
+
+    def menggonggong(self):
+        print("guk .... gukk ..!")
+
+    def info(self):
+        print("Nama Anjing : " + self.nama + ",umurnya =" + str(self.umur) + "tahun,warnanya = " + str(self.warna)
+
+    def berlari(self):
+        print("cepat ....sekali")
---
+++ bab_2_membuat_kelas,atribut_dan_metode/readme.md
@@ -0,0 +1,5 @@
+## 2.5 Tugas Di Kelas
+1. Buatlah Implementasi kelas Menu , isilah method dengan kreasi anda sendiri
+2. Buatlah Implementasi kelas Buku , isilah method dengan kreasi anda sendiri
+3. Buatlah Implementasi kelas Garis , isilah method dengan kreasi anda sendiri
+4. Buatlah Implementasi kelas Pelajar , isilah method dengan kreasi anda sendiri
